ee2b06f0cc9499c5f4a2c3cf63cba4871d0d5ecd
Rofifah Nurul Annisa <rofifahnurul00@gmail.com>
2022-09-15 13:53:25 +0800

    Add rules check

--- aprioriScratch.py
+++ aprioriScratch.py
@@ -1,3 +1,21 @@
+
+import numpy as np
+def create_check(cluster,column_name,data):
+  result = []
+  for value in cluster[column_name]:
+    avg = cluster[column_name].mean()
+    if float(value) >= float(avg):
+      string = str(column_name)+">=" + str(cluster[column_name].mean())
+      result.append(string) 
+      
+    else:
+      string = str(column_name)+"<" + str(cluster[column_name].mean())
+      result.append(string)
+  column_new = str(column_name) + "_check"
+  cluster[column_new] = result 
+  data[column_new]=result
+
+
 def create_candidate_1(X):
     c1 = []
     for transaction in X:
@@ -9,19 +27,22 @@ def create_candidate_1(X):
 
 def aprioriFunc(X, min_support):
     c1 = create_candidate_1(X)
-    freq_item, item_support_dict = create_freq_item(X, c1, min_support)
+    freq_item, item_support_dict,freq_item_support_dict = create_freq_item(X, c1, min_support)
     freq_items = [freq_item]
     
     k = 0
+
     while len(freq_items[k]) > 0:
         freq_item = freq_items[k]
         ck = create_candidate_k(freq_item, k)       
-        freq_item, item_support = create_freq_item(X, ck, min_support)
+        freq_item, item_support,freq_item_support = create_freq_item(X, ck, min_support)
         freq_items.append(freq_item)
+        freq_item_support_dict.update(item_support)
         item_support_dict.update(item_support)
+        
         k += 1
         
-    return freq_items, item_support_dict
+    return freq_items, item_support_dict,freq_item_support
 
 def create_freq_item(X, ck, min_support):
 
@@ -37,15 +58,17 @@ def create_freq_item(X, ck, min_support):
     n_row = X.shape[0]
     freq_item = []
     item_support = {}
+    freq_item_support = {}
 
     for item in item_count:
         support = item_count[item] / n_row
         if support >= min_support:
             freq_item.append(item)
+            freq_item_support[item]=support
         
         item_support[item] = support
         
-    return freq_item, item_support
+    return freq_item, item_support,freq_item_support
 
 from itertools import combinations
 def create_candidate_k(freq_item, k):
@@ -75,15 +98,19 @@ def create_rules(freq_items, item_support_dict, min_confidence):
                                                   freq_set, subsets, min_confidence)
             association_rules.extend(rules)
             
-            if idx != 0:
-                k = 0
-                while len(right_hand_side[0]) < len(freq_set) - 1:
-                    ck = create_candidate_k(right_hand_side, k = k)
-                    rules, right_hand_side = compute_conf(freq_items, item_support_dict,
+            if len(right_hand_side) > 0:
+                try:
+                    if idx != 0:
+                        k = 0
+                        while len(right_hand_side[0]) < len(freq_set) - 1:
+                            ck = create_candidate_k(right_hand_side, k = k)
+                            rules, right_hand_side = compute_conf(freq_items, item_support_dict,
                                                           freq_set, ck, min_confidence)
-                    association_rules.extend(rules)
-                    k += 1    
-    
+                            association_rules.extend(rules)
+                            k += 1    
+                except (IndexError):
+                    msg=1
+            
     return association_rules
 
 def compute_conf(freq_items, item_support_dict, freq_set, subsets, min_confidence):
@@ -103,8 +130,37 @@ def compute_conf(freq_items, item_support_dict, freq_set, subsets, min_confidenc
               conv = "infinity"
             else:
               conv = conv_a/conv_b
-            rules_info = lhs, rhs, conf, lift,conv
+            rules_info = lhs, rhs, conf, lift,conv, item_support_dict[freq_set]
             rules.append(rules_info)
             right_hand_side.append(rhs)
             
     return rules, right_hand_side
+
+
+def generateRules(data,min_support, min_confidence):
+    records = np.array(data)
+    freq_items, item_support_dict,freq_item_support = aprioriFunc(records, min_support)
+    association_rules= create_rules(freq_items, item_support_dict, min_confidence)
+    #len_right = create_rules(freq_items, item_support_dict, min_confidence = 0.8)
+    return association_rules,freq_item_support
+
+def checkRules(rules, data, min_support,min_confidence):
+    while len(rules) > 5:                           
+        min_support = min_support + 0.025
+        rules,freq_item_support = generateRules(data, min_support, min_confidence)
+        print(len(rules),min_support)
+
+        if min_support >= 0.85 and len(rules) > 5:
+            min_confidence = min_confidence +0.01
+            rules,freq_item_support = generateRules(data, min_support, min_confidence)
+            print("kondisi1 ",len(rules),min_support)
+
+            if len(rules) <= 5 or min_support >=0.85:
+                print("kondisi2",len(rules),min_support)
+                break;
+
+            if min_support >=0.85 and min_confidence >0.99 and len(rules) > 5:
+                print("kondisi3",len(rules),min_support)
+                break;
+    
+    return rules, min_support,min_confidence
